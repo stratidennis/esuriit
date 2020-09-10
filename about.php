6dca999aa7fc036a08b9645e17208e49669aa049
stratidennis <46193140+stratidennis@users.noreply.github.com>
2020-09-10 10:41:01 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,34 @@
+<?php
+require 'header.php';
+?>
+
+<main>
+
+    <head>
+        <link rel='stylesheet' href='stylesheet_about.css'>
+        <link href="https://fonts.googleapis.com/css?family=Coda&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div class="welcome">
+            <h1>WELCOME TO ESURIIT!</h1>
+        </div>
+        <p>
+            This is a website designed and built by <a href="https://www.linkedin.com/in/stratinski-dennis" target="_blank">@Stratinski Dennis</a>. It was built as a faculty project in order to be presented at the Web Development class.
+        </p>
+        <p>
+            The website's name, Esuriit, comes from latin and it means being hungry. Short story: this name was given because I was hungry when I've projected the website the first time.
+        </p>
+        <p>
+            Credits: </br>
+            -help from <a href="https://www.facebook.com/grada.silviu" target="_blank">@Silviu Grada</a> in designing the header icons; </br>
+            -the Gravity Points minigame was built and designed by <a href="https://codepen.io/akm2/pen/rHIsa" target="_blank">@Akimitsu Hamamuro</a>.
+        </p>
+        <p>
+            You can contact the developer of this webiste at <a>des.thicklayers@gmail.com</a>.
+        </p>
+
+    </body>
+
+</main>
